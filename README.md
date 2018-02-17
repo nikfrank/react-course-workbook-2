1e1dd88e0854b1dae43c7e8401d40ada3ca03663
nik frank <nikbfrank@gmail.com>
2018-02-17 21:34:05 +0200

    starting point with leaf orbit

---
+++ README.md
@@ -0,0 +1,21 @@
+# react course workbook 2
+
+Here we'll work with user input for the first time
+
+React treats inputs a bit differently than maybe we're used to:
+
+JQuery: (user input -> value in DOM; submit -> read DOM input value -> trigger API call)
+
+React: (user input -> value saved in state; submit -> read from state, trigger API call)
+
+We have plenty of examples to work through here.
+
+Also check out my funky SVG animation!
+
+
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+snoop svg Created by Sage McElroy from the Noun Project
+
+leaf svg from https://upload.wikimedia.org/wikipedia/commons/4/44/Cannabis_leaf_2.svg
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import leaf from './leaf.svg';
+import Snoop from './snoop.svg';
+import './App.css';
+
+class App extends Component {
+  state = {}
+  
+  render() {
+    return (
+      <div className="Apply">
+        <header className="Apply-header">
+          <img src={leaf} className="leaf-logo" alt="leaf" />
+          <Snoop color='purple' faceColor='white' className='snoop-logo'/>
+        </header>
+        <div className="Apply">
+          <h3>Snoop needs some deets to get started</h3>
+          <div className='form-field'>
+            <label for='rap-name'>Rap Name</label>
+            <input id='rap-name' placeholder='Rap Name'/>
+          </div>
+
+          <div className='form-field'>
+            <label for='album-sales'>Current Album Sales (weekly)</label>
+            <input type='number' value={0}/>
+          </div>
+
+          <button>Submit</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
