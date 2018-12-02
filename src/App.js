b5bd0c1ab900b4368f015b0f7b8c3e3956c83ad2
nik frank <nikbfrank@gmail.com>
2018-12-01 16:26:40 -0800

    override for starting step 2

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ class App extends Component {
   state = {
     rapName: '',
     albumSales: 0,
+    applyingFor: '',
+    country: '',
+    whichState: '',
   }
 
   setRapName = (event)=> this.setState({ rapName: event.target.value })
   setAlbumSales = (event)=> this.setState({ albumSales: +event.target.value })
+  setApplyingFor = (event)=> this.setState({ applyingFor: event.target.value })
+  setCountry = (event)=> this.setState({ country: event.target.value })
+  setWhichState = (event)=> this.setState({ whichState: event.target.value })
   
   submitForm = ()=>{
     console.log( this.state );
@@ -44,6 +50,38 @@ class App extends Component {
             </label>
           </div>
 
+          <div className='form-field'>
+            <select value={this.state.applyingFor} onChange={this.setApplyingFor}>
+              <option value=''>Select Position</option>
+              <option value='driver'>Driver</option>
+              <option value='trafficker'>Trafficker</option>
+              <option value='producer'>Producer</option>
+              <option value='rapper'>Rapper</option>
+            </select>
+          </div>
+
+          <div className='form-field'>
+            <select value={this.state.country} onChange={this.setCountry}>
+              <option value=''>Select Country</option>
+              <option value='USA'>USA</option>
+              <option value='Canada'>Canada</option>
+              <option value='Israel'>Israel</option>
+            </select>
+          </div>
+
+          {
+            (this.state.country === 'USA') ? (
+              <div className='form-field'>
+                <select value={this.state.whichState} onChange={this.setWhichState}>
+                  <option value=''>Select State</option>
+                  <option value='CA'>California</option>
+                  <option value='NY'>New York</option>
+                  <option value='MI'>Michigan</option>
+                </select>
+              </div>
+            ) : null
+          }
+          
           <button onClick={this.submitForm}>Submit</button>
         </div>
       </div>
